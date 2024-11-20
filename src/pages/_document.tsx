7f14cae85a9fd1bc9bd35610e8ff4fc719ba0d6a
Isaac Alencar <alencarisaac3@gmail.com>
2024-11-20 10:45:31 -0300

    remove some stuff from nextjs initialization

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Shopping Cart</title>
       <body className="antialiased">
         <Main />
         <NextScript />
