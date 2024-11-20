cd5bfcdf542a76b9a9e4a943680d0e21908b3f3c
Isaac Alencar <alencarisaac3@gmail.com>
2024-11-20 15:35:48 -0300

    refactoring domain types
    introduce CartItem as items on cart and add id on products

--- src/modules/shopping/domain/Cart.ts
+++ src/modules/shopping/domain/Cart.ts
@@ -1,7 +1,12 @@
 import { Product } from "./Product";
 
+export type CartItem = {
+  product: Product;
+  quantity: number;
+};
+
 export type Cart = {
-  items: Product;
+  items: CartItem[];
   totalItemsPrice: number;
   totalPrice: number;
   quantity: number;
